700ce87839af3741849a2bb8c1b2ae5d16e482db
limasuu <szlima.93@gmail.com>
2022-12-12 10:22:34 -0300

    Criando a área de visualização

--- src/index.js
+++ src/index.js
@@ -19,6 +19,16 @@ function Visualizador(){
                 </div>
                 <textarea id='editor' value={codigo} onChange={e => setCodigo(e.target.value)}></textarea>
             </div>
+
+            <div className='janela' id='janelaPreview'>
+                <div className='topo'>
+                    <span><i className="fa-brands fa-free-code-camp"></i> Previewer</span>
+                    <i className="fa-solid fa-maximize" id='botaoPreview'></i>         
+                </div>      
+                <div id='preview' dangerouslySetInnerHTML={{
+            __html: window.marked.parse(codigo, {breaks:true})}}></div>
+            </div>
+
       </React.Fragment>
     );
 }
